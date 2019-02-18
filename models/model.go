93837de0956562afae012c7647e83ebe67a07b34
장지훈 <s5v7@outlook.kr>
2019-02-19 01:14:48 +0900

    users, applys, holydays Table Model 추가

--- models/model.go
+++ models/model.go
@@ -1,2 +1,64 @@
 ﻿package models
 
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	// SQLConnectionString : MySQL Connection String
+	SQLConnectionString = "*"
+	// SALT : SALT
+	SALT = "*"
+)
+
+// Database Connection
+var db *gorm.DB
+
+func init() {
+	var err error
+	db, err = gorm.Open("mysql", SQLConnectionString)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Apply models
+type Apply struct {
+	StudentNumber string    `gorm:"type:VARCHAR(6); primary_key" json:"student-number"`
+	Name          string    `gorm:"type:VARCHAR(50)" json:"name"`
+	Date          time.Time `gorm:"type:DATE; primary_key; unique_index" json:"date"`
+	Time          string    `gorm:"type:VARCHAR(5); primary_key; unique_index" json:"time"`
+	Type          string    `gorm:"type:VARCHAR(1)" json:"type"`
+	Seat          string    `gorm:"type:VARCHAR(6); unique_index" json:"seat"`
+}
+
+// TableName of Apply
+func (c *Apply) TableName() string {
+	return "applys"
+}
+
+// User models
+type User struct {
+	StudentNumber string `gorm:"type:VARCHAR(6); primary_key" json:"student-number"`
+	Password      string `gorm:"type:VARCHAR(100)" json:"password"`
+	Name          string `gorm:"type:VARCHAR(50)" json:"name"`
+	Gender        int    `gorm:"type:INT" json:"gender"`
+}
+
+// TableName of User
+func (c *User) TableName() string {
+	return "users"
+}
+
+// Holyday models
+type Holyday struct {
+	Date time.Time `gorm:"type:DATE; primary_key" json:"date"`
+	Name string    `gorm:"type:VARCHAR(50)" json:"name"`
+}
+
+// TableName of Holyday
+func (c *Holyday) TableName() string {
+	return "holydays"
+}
